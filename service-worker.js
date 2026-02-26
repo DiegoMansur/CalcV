7f83412e77c4853d74325c90f9f81a611846cc5c
DiegoMansur <diegofacundo1610@gmail.com>
2026-02-25 20:50:57 -0500

    Enhance service worker fetch event handling
    Updated fetch event to return index.html on failure.

--- service-worker.js
+++ service-worker.js
@@ -35,9 +35,10 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request);
+        return response || fetch(event.request).catch(() => caches.match("./index.html"));
       })
   );
 });
+
 
 
